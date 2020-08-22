9c27759cbecf9c656e2a1036f5b777605d974238
Adesina-123 <adesinaoladimeji92@gmail.com>
2020-08-22 19:00:47 +0200

    Updates

---
+++ precache-manifest.6fcdfb486864f19a0811d00df7812f71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1be5036e386a6ead5e52899d67783ac",
+    "url": "/github.io/react-tutorial-1/index.html"
+  },
+  {
+    "revision": "627d06aa5fa96e6cd191",
+    "url": "/github.io/react-tutorial-1/static/css/main.46e63255.chunk.css"
+  },
+  {
+    "revision": "e6be6f09235467cc57e6",
+    "url": "/github.io/react-tutorial-1/static/js/2.3f6359c6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/github.io/react-tutorial-1/static/js/2.3f6359c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "627d06aa5fa96e6cd191",
+    "url": "/github.io/react-tutorial-1/static/js/main.b3f963d6.chunk.js"
+  },
+  {
+    "revision": "46dd69cbee7249c319c2",
+    "url": "/github.io/react-tutorial-1/static/js/runtime-main.aa32e636.js"
+  }
+]);
